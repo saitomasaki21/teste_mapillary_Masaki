408d1f7d2e92d678075da895c794184f6ccf06cb
saitomasaki21 <100047297+saitomasaki21@users.noreply.github.com>
2024-10-01 16:21:23 -0400

    Update MapillaryViewer.jsx

--- src/components/MapillaryViewer.jsx
+++ src/components/MapillaryViewer.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useRef } from 'react';
 import { Viewer } from 'mapillary-js';
 
 export const MapillaryViewer = ({ accessToken, imageId, viewerRef }) => {
-  const mapillaryContainerRef = new mapboxgl.Map({
-        container: 'map',
-        zoom: 16.2,
-        pitch: 75,
-        bearing: -170,
-        center: [-122.395, 37.792],
-        style: 'mapbox://styles/mapbox/standard', // Use the Mapbox Standard style
-        config: {
-            // Initial configuration for the Mapbox Standard style set above. By default, its ID is `basemap`.
-            basemap: {
-                // Here, we're disabling all of the 3D layers such as landmarks, trees, and 3D extrusions.
-                show3dObjects: false
-            }
-        }
+  const mapillaryContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (!mapillaryContainerRef.current) return;
+
+    viewerRef.current = new Viewer({
+      accessToken,
+      container: mapillaryContainerRef.current,
+      imageId,
+      component: {
+        cover: false,
+        direction: false,
+        sequence: false,
+        zoom: false,
+      },
     });
 
     return () => {
