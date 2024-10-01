8640a7b6af1d59dc5f349000bf7ff83dbd04234e
saitomasaki21 <100047297+saitomasaki21@users.noreply.github.com>
2024-10-01 16:59:10 -0400

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,7 +6,7 @@ const Index = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="text-center mb-8">
         <h1 className="text-4xl font-bold mb-4">Bem vindos ao Teste utilizando Mapillary e Mapbox </h1>
-        <p className="text-xl text-gray-600">Testes executados no âmbito do Campus Map UFAM</p>
+        <p className="text-xl text-gray-600">Editado por MASAKI </p>
       </div>
       <Link
         to="/map"
