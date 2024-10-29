f1d2354bd2b381625a4ae66a5d269a8fca78bbb5
saitomasaki21 <100047297+saitomasaki21@users.noreply.github.com>
2024-10-29 13:01:27 -0400

    Update mapData.js

--- src/utils/mapData.js
+++ src/utils/mapData.js
@@ -1,62 +1,13 @@
-mbx.addSource(532582459129057,
-              1548741512383121,
-              1452951302053880,
-              2474641246068877, {
+export const coordinates = [
+  [-59.97653145659109, -3.1010197521129896],
+  [-59.97653145659109, -3.1010197521129896],
+  [-59.97653145659109, -3.1010197521129896],
+  [-59.97653145659109, -3.1010197521129896]
+];
 
-  type: 'geojson',
-
-  data: {
-
-    type: 'FeatureCollection',
-
-    features: data.data.map(image => ({
-
-      type: 'Feature',
-
-      geometry: {
-
-        type: 'Point',
-
-        coordinates: image.geometry.coordinates
-
-      },
-
-      properties: {
-
-        id: image.id,
-
-        isCurrentImage: image.id === event.image.id
-
-      }
-
-    }))
-
-  }
-
-});
-
-
-
-mbx.addLayer({
-
-  id: 'sequence-points',
-
-  type: 'circle',
-
-  source: 'sequence-points',
-
-  paint: {
-
-    'circle-radius': 6,
-
-    'circle-color': ['case', ['==', ['get', 'isCurrentImage'], true], 'red', 'blue'],
-
-    'circle-stroke-width': 2,
-
-    'circle-stroke-color': 'white'
-
-  }
-
-});
-
+export const imageIds = [
+  532582459129057,
+  1548741512383121,
+  1452951302053880,
+  2474641246068877
 ];
