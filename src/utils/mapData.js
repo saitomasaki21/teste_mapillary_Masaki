6012bc31d6da42f7b415180f6304206346d3885c
saitomasaki21 <100047297+saitomasaki21@users.noreply.github.com>
2024-11-05 14:32:55 -0400

    Update mapData.js

--- src/utils/mapData.js
+++ src/utils/mapData.js
@@ -1,4 +1,3 @@
-// Array com os imageIds do Mapillary
 export const imageIds = [
   532582459129057,
   1548741512383121,
@@ -6,38 +5,9 @@ export const imageIds = [
   2474641246068877
 ];
 
-// Variável para armazenar as coordenadas
-let coordinates = [];
-
-// Função para obter as coordenadas a partir do Mapillary
-async function fetchCoordinates(imageIds) {
-  const coords = [];
-
-  for (const id of imageIds) {
-    try {
-      const response = await fetch(`https://a.mapillary.com/v3/images/${id}?client_id=YOUR_CLIENT_ID`);
-      if (!response.ok) {
-        throw new Error(`Erro ao buscar a imagem com ID ${id}: ${response.statusText}`);
-      }
-      const data = await response.json();
-      // Adiciona as coordenadas ao array
-      coords.push({
-        latitude: data.lat,
-        longitude: data.lon,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  return coords;
-}
-
-// Função para inicializar as coordenadas
-export async function initCoordinates() {
-  coordinates = await fetchCoordinates(imageIds);
-}
-
-// Exporta a variável de coordenadas
-export const getCoordinates = () => coordinates;
-
+export const coordinates = [
+  [-49.2324409584695, -25.4508140632974],
+  [-49.23244804, -25.45083001],
+  [-49.23245823246808, -25.450847256519253],
+  [-49.2324689642478, -25.4508428103442]
+  ];
